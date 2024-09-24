c960865dee440276ad12fb9fcdcde53b1e3c0463
satoaki1 <ihsa2019@hotmail.com>
2024-09-24 16:16:11 +0800

    Initial Commit

---
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by 石原 聡明 on 2024/09/24.
+//
+
+import Testing
+@testable import Scrumdinger
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
